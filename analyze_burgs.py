59d416ab3123db21aa6b3c71ebd0595b712c2288
Imanol Irizar Da Silva <imanol.irizar13@gmail.com>
2025-11-24 16:38:26 +0100

    rework analysis

--- analyze_burgs.py
+++ analyze_burgs.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
-burg_models = pd.read_json('burgs.json')
-df_burgs = pd.DataFrame.from_records(burg_models)
+burgs = pd.read_json('data/burgs.json')
+df_burgs = pd.DataFrame.from_records(burgs)
+
 df_burgs_net_production = pd.DataFrame.from_records(df_burgs.net_production_burg)
+df_burgs_net_production = pd.concat([df_burgs[['name','type','capital','nr_quartiers']], df_burgs_net_production], axis=1)
+df_burgs_net_production.sort_values(by='Net_Food', ascending=False)
+df_burgs_net_production.to_csv('data/burgs_net_production.csv')
 
-
-
+df_burgs.sort_values(by='nr_quartiers', ascending=False)
+df_burgs.to_csv('data/burgs.csv')
 
 print(f'\tTotal world production:\n{repr(df_burgs_net_production.sum())}')
 print(f'\tTotal world quartiers: {df_burgs.nr_quartiers.sum()}')
